9f071a8b449715bf4255f1c93a2a87fac835647c
ahincho <ahincho@unsa.edu.pe>
2024-10-07 16:10:23 -0500

    Flux Skip Examples

--- Webflux/Genuine/Essentials/src/main/java/com/learning/spring/webflux/essentials/ReactiveTutorial.java
+++ Webflux/Genuine/Essentials/src/main/java/com/learning/spring/webflux/essentials/ReactiveTutorial.java
@@ -3,6 +3,7 @@ package com.learning.spring.webflux.essentials;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +34,24 @@ public class ReactiveTutorial {
     private Mono<String> addWord(String string) {
         return Mono.just(String.format("%s Course", string)).log();
     }
+    private Flux<String> testSkipCount() {
+        Flux<String> flux = Flux.fromIterable(List.of("Java", "Spring", "Spring Webflux")).log();
+        return flux.skip(2);
+    }
+    private Flux<String> testSkipDuration() {
+        Flux<String> flux = Flux.fromIterable(List.of("Java", "Quarkus", "Spring", "Spring Webflux", "Spring MVC"))
+                .delayElements(Duration.ofSeconds(1));
+        return flux.skip(Duration.ofSeconds(2)).log();
+    }
+    private Flux<String> testSkipLast() {
+        Flux<String> flux = Flux.fromIterable(List.of("Java", "Quarkus", "Spring", "Spring Webflux", "Spring MVC"));
+        return flux.skipLast(2).log();
+    }
+    private Flux<Integer> testSkipComplex() {
+        Flux<Integer> flux = Flux.range(1, 20);
+        // return flux.skipWhile(integer -> integer < 10).log();
+        return flux.skipUntil(integer -> integer == 30).log();
+    }
     public static void main(String[] args) {
         ReactiveTutorial reactiveTutorial = new ReactiveTutorial();
         System.out.println("Mono Just");
@@ -49,5 +68,18 @@ public class ReactiveTutorial {
         reactiveTutorial.testFluxMap().subscribe(System.out::println);
         System.out.println("Flux FlatMap");
         reactiveTutorial.testFluxFlatMap().subscribe(System.out::println);
+        System.out.println("Flux Skip");
+        reactiveTutorial.testSkipCount().subscribe(System.out::println);
+        System.out.println("Flux Skip Duration");
+        reactiveTutorial.testSkipDuration().subscribe(System.out::println);
+        try {
+            Thread.sleep(10_000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Flux Skip Last");
+        reactiveTutorial.testSkipLast().subscribe(System.out::println);
+        System.out.println("Flux Skip Complex");
+        reactiveTutorial.testSkipComplex().subscribe(System.out::println);
     }
 }
