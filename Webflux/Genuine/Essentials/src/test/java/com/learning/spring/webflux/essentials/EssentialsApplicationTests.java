bd3655c74106aa116569c99e122199bc8d09365c
ahincho <ahincho@unsa.edu.pe>
2024-10-07 15:04:17 -0500

    Start Webflux Project

---
+++ Webflux/Genuine/Essentials/src/test/java/com/learning/spring/webflux/essentials/EssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.webflux.essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
