5c21d743773fcfb72050409f0ec17bc66038ff5e
ahincho <ahincho@unsa.edu.pe>
2024-10-07 15:22:00 -0500

    Mono or Empty Test

--- Webflux/Genuine/Essentials/src/main/java/com/learning/spring/webflux/essentials/ReactiveTutorial.java
+++ Webflux/Genuine/Essentials/src/main/java/com/learning/spring/webflux/essentials/ReactiveTutorial.java
@@ -2,12 +2,18 @@ package com.learning.spring.webflux.essentials;
 
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 public class ReactiveTutorial {
-    private Mono<String> testMono() {
-        return Mono.just("Webflux");
+    private Mono<String> testMonoJust() {
+        return Mono.just("Webflux").log();
+    }
+    private Mono<Object> testMonoJustOrEmpty() {
+        return Mono.justOrEmpty(Optional.empty()).log();
     }
     public static void main(String[] args) {
         ReactiveTutorial reactiveTutorial = new ReactiveTutorial();
-        reactiveTutorial.testMono().subscribe(System.out::println);
+        reactiveTutorial.testMonoJust().subscribe(System.out::println);
+        reactiveTutorial.testMonoJustOrEmpty().subscribe(System.out::println);
     }
 }
