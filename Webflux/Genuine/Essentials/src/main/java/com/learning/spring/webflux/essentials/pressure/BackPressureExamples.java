66428b9710f6c003c297398154798f6b2f067688
ahincho <ahincho@unsa.edu.pe>
2024-10-07 22:18:37 -0500

    No Overflow Example

---
+++ Webflux/Genuine/Essentials/src/main/java/com/learning/spring/webflux/essentials/pressure/BackPressureExamples.java
@@ -0,0 +1,17 @@
+package com.learning.spring.webflux.essentials.pressure;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+public class BackPressureExamples {
+    private static Flux<Long> createNoOverflowFlux() {
+        return Flux.range(1, Integer.MAX_VALUE)
+                .log()
+                .concatMap(x -> Mono.delay(Duration.ofMillis(100)));
+    }
+    public static void main(String[] args) {
+        createNoOverflowFlux().blockLast();
+    }
+}
