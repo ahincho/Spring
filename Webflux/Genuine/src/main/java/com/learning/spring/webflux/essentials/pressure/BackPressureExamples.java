ffff23c7bd898961537718f00096941e4d29cf0d
ahincho <ahincho@unsa.edu.pe>
2024-10-08 08:52:58 -0500

    OnBackPressure Examples

--- Webflux/Genuine/src/main/java/com/learning/spring/webflux/essentials/pressure/BackPressureExamples.java
+++ Webflux/Genuine/src/main/java/com/learning/spring/webflux/essentials/pressure/BackPressureExamples.java
@@ -1,5 +1,6 @@
 package com.learning.spring.webflux.essentials.pressure;
 
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,7 +17,21 @@ public class BackPressureExamples {
                 .log()
                 .concatMap(x -> Mono.delay(Duration.ofMillis(100)));
     }
+    private static Flux<Long> onBackPressureDropIt() {
+        return Flux.interval(Duration.ofMillis(10))
+                .onBackpressureDrop()
+                .log()
+                .concatMap(x -> Mono.delay(Duration.ofMillis(100)).thenReturn(x))
+                .doOnNext(System.out::println);
+    }
+    private static Flux<Long> onBackPressureBuffer() {
+        return Flux.interval(Duration.ofMillis(10))
+                .onBackpressureBuffer(50, BufferOverflowStrategy.DROP_OLDEST)
+                .log()
+                .concatMap(x -> Mono.delay(Duration.ofMillis(100)).thenReturn(x))
+                .doOnNext(System.out::println);
+    }
     public static void main(String[] args) {
-        createOverflowFlux().blockLast();
+        onBackPressureBuffer().blockLast();
     }
 }
